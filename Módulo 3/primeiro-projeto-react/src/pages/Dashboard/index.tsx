4cac24911d36b51143a99fe862c4ae380272774a
IsabellaSoares <isabella.soares.28@gmail.com>
2021-02-18 13:16:16 -0300

    style Dashboard

--- Módulo 3/primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ Módulo 3/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
-import { Title } from './styles';
+import logo from '../../assets/logo.svg';
 
-const Dashboard: React.FC = () => (
-  <Title>Explore repositórios no Github.</Title>
-);
+import { Title, Form } from './styles';
+
+const Dashboard: React.FC = () => {
+  return (
+    <>
+      <img src={logo} alt="Github Explorer" />
+      <Title>Explore repositórios no Github.</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+    </>
+  );
+};
 
 export default Dashboard;
