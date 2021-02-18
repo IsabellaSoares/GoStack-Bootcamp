a53efaac1769372e0a69d4f37b0ee10b9f565571
IsabellaSoares <isabella.soares.28@gmail.com>
2021-02-18 14:38:43 -0300

    add Repositories component

--- Módulo 3/primeiro-projeto-react/src/pages/Dashboard/index.tsx
+++ Módulo 3/primeiro-projeto-react/src/pages/Dashboard/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logo from '../../assets/logo.svg';
 
-import { Title, Form } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
@@ -14,6 +15,22 @@ const Dashboard: React.FC = () => {
         <input placeholder="Digite o nome do repositório" />
         <button type="submit">Pesquisar</button>
       </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://avatars.githubusercontent.com/u/32148199?s=400&u=0caa8c2990249248222f86ac11d439d03adfa6d6&v=4"
+            alt="Isabella Soares"
+          />
+
+          <div>
+            <strong>GoStack-Bootcamp/Módulo 3</strong>
+            <p>Primeiro projeto em React</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
