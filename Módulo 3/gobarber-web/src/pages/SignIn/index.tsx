2f156fb52238d9bc6ba434910e47336e8b759192
IsabellaSoares <isabella.soares.28@gmail.com>
2021-02-28 17:25:00 -0300

    add auth provider

--- Módulo 3/gobarber-web/src/pages/SignIn/index.tsx
+++ Módulo 3/gobarber-web/src/pages/SignIn/index.tsx
@@ -1,10 +1,11 @@
-import React, { useRef, useCallback } from 'react';
+import React, { useRef, useCallback, useContext } from 'react';
 import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
 import getValidationErrors from '../../utils/getValidationErrors';
+import { AuthContext } from '../../context/AuthContext';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
@@ -12,12 +13,21 @@ import Button from '../../components/Button';
 import { Container, Content, Background } from './styles';
 
 import logoImg from '../../assets/logo.svg';
+import { sign } from 'crypto';
+
+interface SignInFormData {
+  email: string;
+  password: string;
+}
 
 const SignIn: React.FC = () => {
   const formRef = useRef<FormHandles>(null);
+  const { signIn } = useContext(AuthContext);
 
-  const handleSubmit = useCallback(async (data: object) => {
+  const handleSubmit = useCallback(async (data: SignInFormData) => {
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
         password: Yup.string().required('Senha obrigatória')
@@ -25,6 +35,11 @@ const SignIn: React.FC = () => {
 
       await schema.validate(data, {
         abortEarly: false,
+      });
+
+      signIn({
+        email: data.email,
+        password: data.password
       });
     } catch (err) {
       if(err instanceof Yup.ValidationError){
@@ -35,7 +50,7 @@ const SignIn: React.FC = () => {
 
       formRef.current?.setErrors(errors);
     }
-  }, []);
+  }, [signIn]);
 
   return (
     <Container>
