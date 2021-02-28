ae3f175daf25e757bee503db663f121d261b05db
IsabellaSoares <isabella.soares.28@gmail.com>
2021-02-28 14:37:02 -0300

    add errors tooltip

--- Módulo 3/gobarber-web/src/components/Input/index.tsx
+++ Módulo 3/gobarber-web/src/components/Input/index.tsx
@@ -1,8 +1,9 @@
 import React, { InputHTMLAttributes, useRef, useEffect, useState, useCallback } from 'react';
 import { IconBaseProps } from 'react-icons';
+import { FiAlertCircle } from 'react-icons/fi';
 import { useField } from '@unform/core';
 
-import { Container } from './styles';
+import { Container, Error } from './styles';
 
 interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
   name: string;
@@ -21,7 +22,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
       ref: inputRef.current,
       path: 'value'
     });
-  }, []);
+  }, [fieldName, registerField]);
 
   const handleInputFocus = useCallback(() => {
     setIsFocused(true);
@@ -33,7 +34,7 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
   }, []);
 
   return (
-    <Container isFocused={isFocused} isFilled={isFilled}>
+    <Container isErrored={!!error} isFocused={isFocused} isFilled={isFilled}>
       {Icon && <Icon size={20} />}
       <input
         onFocus={handleInputFocus}
@@ -42,6 +43,8 @@ const Input: React.FC<InputProps> = ({ name, icon: Icon, ...rest }) => {
         ref={inputRef}
         {...rest}
       />
+
+      {error && <Error title={error}><FiAlertCircle color='#c53030' size={20} /></Error>}
     </Container>
   )
 };
--- Módulo 3/gobarber-web/src/components/Input/styles.ts
+++ Módulo 3/gobarber-web/src/components/Input/styles.ts
@@ -1,6 +1,9 @@
 import styled, { css } from 'styled-components';
 
+import Tooltip from '../Tooltip';
+
 interface ContainerProps {
+  isErrored: boolean;
   isFocused: boolean;
   isFilled: boolean;
 }
@@ -18,6 +21,10 @@ export const Container = styled.div<ContainerProps>`
   & + div {
     margin-top: 8px;
   }
+
+  ${props => props.isErrored && css`
+    border-color: #c53030;
+  `}
 
   ${props => props.isFocused && css`
     color: #ff9000;
@@ -41,5 +48,23 @@ export const Container = styled.div<ContainerProps>`
 
   svg {
     margin-right: 16px;
+  }
+`;
+
+export const Error = styled(Tooltip)`
+  height: 20px;
+  margin-left: 16px;
+
+  svg {
+    margin: 0;
+  }
+
+  span {
+    background: #c53030;
+    color: #fff;
+
+    &::before {
+      border-color: #c53030 transparent;
+    }
   }
 `;
