484623165948941990c4551dd076c62a8d20a364
IsabellaSoares <isabella.soares.28@gmail.com>
2021-03-05 19:12:34 -0300

    add form authentication

--- Modulo 3/appgobarber/src/components/Input/index.tsx
+++ Modulo 3/appgobarber/src/components/Input/index.tsx
@@ -1,4 +1,11 @@
-import React, { useEffect, useRef, useImperativeHandle, forwardRef } from 'react';
+import React, {
+  useEffect,
+  useRef,
+  useImperativeHandle,
+  forwardRef,
+  useState,
+  useCallback
+} from 'react';
 import { TextInputProps } from 'react-native';
 import { useField } from '@unform/core';
 
@@ -21,6 +28,8 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = ({ name, ico
   const inputElementRef = useRef<any>(null);
   const { registerField, defaultValue = '', fieldName, error } = useField(name);
   const inputValueRef = useRef<InputValueReference>({ value: defaultValue });
+  const [isFocused, setIsFocused] = useState(false);
+  const [isFilled, setIsFilled] = useState(false);
 
   useImperativeHandle(ref, () => ({
     focus() {
@@ -44,14 +53,25 @@ const Input: React.ForwardRefRenderFunction<InputRef, InputProps> = ({ name, ico
     });
   }, [fieldName, registerField]);
 
+  const handleInputBlur = useCallback(() => {
+    setIsFocused(false);
+    setIsFilled(!!inputValueRef.current.value);
+  }, []);
+
+  const handleInputFocus = useCallback(() => {
+    setIsFocused(true);
+  }, []);
+
   return (
-    <Container>
-      <Icon name={icon} size={20} color='#666360' />
+    <Container isFocused={isFocused} isErrored={!!error}>
+      <Icon name={icon} size={20} color={isFocused || isFilled ? '#ff9000' : '#666360'} />
       <TextInput
         ref={inputElementRef}
         keyboardAppearance='dark'
         placeholderTextColor='#666360'
         defaultValue={defaultValue}
+        onFocus={handleInputFocus}
+        onBlur={handleInputBlur}
         onChangeText={value => {
           inputValueRef.current.value = value;
         }}
--- Modulo 3/appgobarber/src/pages/SignUp/index.tsx
+++ Modulo 3/appgobarber/src/pages/SignUp/index.tsx
@@ -1,16 +1,33 @@
 import React, { useRef, useCallback } from 'react';
-import { Image, View, ScrollView, KeyboardAvoidingView, Platform, TextInput } from 'react-native';
+import {
+  Image,
+  View,
+  ScrollView,
+  KeyboardAvoidingView,
+  Platform,
+  TextInput,
+  Alert
+} from 'react-native';
 import Icon from 'react-native-vector-icons/Feather';
 import { useNavigation } from '@react-navigation/native';
 import { Form } from '@unform/mobile';
 import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
 
 import { Container, Title, BackToSignInButton, BackToSignInText } from './styles';
+
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import Input from '../../components/Input';
 import Button from '../../components/Button';
 
 import logoImg from '../../assets/logo.png';
+
+interface SignUpFormData {
+  name: string;
+  email: string;
+  password: string;
+}
 
 const SignUp: React.FC = () => {
   const navigation = useNavigation();
@@ -18,8 +35,36 @@ const SignUp: React.FC = () => {
   const emailInputRef = useRef<TextInput>(null);
   const passwordInputRef = useRef<TextInput>(null);
 
-  const handleSubmit = useCallback((data: object) => {
-    console.log(data);
+  const handleSubmit = useCallback(async (data: SignUpFormData) => {
+    try {
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Nome obrigatório'),
+        email: Yup.string().required('E-mail obrigatório').email('Digite um e-mail válido'),
+        password: Yup.string().min(6, 'Mínimo de 6 dígitos')
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+      // await api.post('/users', data);
+
+      // history.push('/');
+
+      // addToast({
+      //   type: 'success',
+      //   title: 'Cadastro realizado!',
+      //   description: 'Você já pode fazer seu logon no GoBarber!'
+      // });
+    } catch (err) {
+      if(err instanceof Yup.ValidationError){
+        const errors = getValidationErrors(err);
+
+        formRef.current?.setErrors(errors);
+      }
+
+      Alert.alert('Erro no cadastro', 'Ocorreu um erro ao fazer cadastro, tente novamente.');
+    }
   }, []);
 
   return (
